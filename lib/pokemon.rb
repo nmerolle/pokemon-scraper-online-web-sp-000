6c79919b31bf4d0e2787e50ae7b8a4acb40121cc
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-07 03:44:14 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :dbm, :hp
+  
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type) VALUES (?, ?);
+    SQL
+    
+    db.execute(sql, name, type)
+    
+  end
+  
+  
 end
